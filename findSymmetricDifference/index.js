f2b5b2d1fad81e71c5b6d661a7a91b1b3e458dad
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2025-11-16 19:08:54 +0700

    update: findSymmetricDifference

--- findSymmetricDifference/index.js
+++ findSymmetricDifference/index.js
@@ -14,3 +14,35 @@ function sym(...args) {
 }
 
 console.log(sym([1, 2, 3], [5, 2, 1, 4]));
+
+// Alternative solution
+// function sym(...args) {
+//   const reduceResult = args.reduce(getSymDifference);
+
+//   const uniqueResult = [...new Set(reduceResult)];
+//   const sortedResult = uniqueResult.sort((a, b) => a - b);
+
+//   return sortedResult;
+// }
+
+// function getSymDifference(arrA, arrB) {
+//   const setA = new Set(arrA);
+//   const setB = new Set(arrB);
+//   const result = [];
+
+//   for (const element of setA) {
+//     if (!setB.has(element)) {
+//       result.push(element);
+//     }
+//   }
+
+//   for (const element of setB) {
+//     if (!setA.has(element)) {
+//       result.push(element);
+//     }
+//   }
+
+//   return result;
+// }
+
+// console.log(sym([1, 2, 3], [2, 3, 4]));
