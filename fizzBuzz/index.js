2561b9cc306154f3d9765c99bbc33277d89c07d3
iqbalzayn01 <iqbalzayyanabiyyu@gmail.com>
2025-11-23 17:04:50 +0700

    update: fizz buzz

---
+++ fizzBuzz/index.js
@@ -0,0 +1,18 @@
+function fizzBuzz(n) {
+  let arr = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      arr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      arr.push('Fizz');
+    } else if (i % 5 === 0) {
+      arr.push('Buzz');
+    } else {
+      arr.push(i.toString());
+    }
+  }
+  console.log(arr);
+}
+
+fizzBuzz(5);
